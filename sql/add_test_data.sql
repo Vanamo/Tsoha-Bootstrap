345aded123b67f3d072b3e81a480694626ab7365
vanamo.piirainen@helsinki.fi <>
2017-09-14 21:32:41 +0300

    Tietokantataulujen luonti ja poisto lisätty, testidataa, muutama keskeneräinen suunnitelma html-sivuista

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,25 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+#Test data for the Customer table
+INSERT INTO Customer (name, password) VALUES ('Vanamo', 'sala123');
+INSERT INTO Customer (name, password) VALUES ('Esko', 'E123O');
+
+#Test data for the Ingredient table
+INSERT INTO Ingredient (name) VALUES ('kananmuna');
+INSERT INTO Ingredient (name) VALUES ('vehnäjauho');
+INSERT INTO Ingredient (name) VALUES ('pakastepinaatti');
+INSERT INTO Ingredient (name) VALUES ('maito');
+INSERT INTO Ingredient (name) VALUES ('suola');
+
+#Test data for the Unit table
+INSERT INTO Unit (name) VALUES ('kpl');
+INSERT INTO Unit (name) VALUES ('rkl');
+INSERT INTO Unit (name) VALUES ('dl');
+
+#Test data for the Tag table
+INSERT INTO Tag (name) VALUES ('kasvisruoka');
+INSERT INTO Tag (name) VALUES ('helppo');
+
+#Test data for the Recipe table
+INSERT INTO Recipe (customer_id, name, instructions)
+    VALUES ((select id from Customer where name = 'Vanamo'), 
+            'Pinaattiletut', 'Sekoita aineet keskenään. Paista 200 asteessa 30 min.');
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,48 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+
+CREATE TABLE Customer(
+    id SERIAL PRIMARY KEY,
+    name varchar(50) NOT NULL,
+    password_hash varchar(50) NOT NULL,
+    salt varchar(10) NOT NULL
+); 
+
+CREATE TABLE Recipe(
+    id SERIAL PRIMARY KEY,
+    customer_id INTEGER REFERENCES Customer(id),
+    name varchar(100) NOT NULL,
+    instructions varchar(5000)
+);
+
+CREATE TABLE Ingredient (
+    id SERIAL PRIMARY KEY,
+    name varchar(50) NOT NULL
+);
+
+CREATE TABLE Unit (
+    id SERIAL PRIMARY KEY,
+    name varchar(20) NOT NULL
+);
+
+CREATE TABLE IngredientOfARecipe (
+    recipe_id INTEGER REFERENCES Recipe(id),
+    ingredient_id INTEGER REFERENCES Ingredient(id),
+    unit_id INTEGER REFERENCES Unit(id),
+    amount DOUBLE,
+    listOrder INTEGER 
+);
+
+CREATE TABLE Tag (
+    id SERIAL PRIMARY KEY,
+    name varchar(100) NOT NULL
+);
+
+CREATE TABLE TagOfARecipe (
+    recipe_id INTEGER REFERENCES Recipe(id),
+    tag_id INTEGER REFERENCES Tag(id)
+);
+
+CREATE TABLE FavoriteRecipe (
+    recipe_id INTEGER REFERENCES Recipe(id),
+    customer_id INTEGER REFERENCES Customer(id)
+);
